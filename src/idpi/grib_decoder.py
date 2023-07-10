7f3e747143879dc0eec561055243d9bb229f55da
Victoria Cherkas <victoria.cherkas@meteoswiss.ch>
2023-07-10 15:54:57 +0200

    formatting

--- src/idpi/grib_decoder.py
+++ src/idpi/grib_decoder.py
@@ -8,10 +8,12 @@ from pathlib import Path
 
 # Third-party
 import earthkit.data  # type: ignore
-import eccodes  # type: ignore
 import numpy as np
 import xarray as xr
 import yaml
+
+# First-party
+import eccodes  # type: ignore
 
 DIM_MAP = {
     "level": "z",
