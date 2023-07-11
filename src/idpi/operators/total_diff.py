267dff43a54146fe73df480993fa27312ae7f746
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2023-07-11 09:52:44 +0200

    refactor total diff class

--- src/idpi/operators/total_diff.py
+++ src/idpi/operators/total_diff.py
@@ -16,19 +16,39 @@ class TotalDiff:
 
     dlon: float
     dlat: float
-    hhl: dc.InitVar[xr.DataArray]
-    sqrtg_r_s: xr.DataArray = dc.field(init=False)
-    dzeta_dlam: xr.DataArray = dc.field(init=False)
-    dzeta_dphi: xr.DataArray = dc.field(init=False)
+    sqrtg_r_s: xr.DataArray
+    dzeta_dlam: xr.DataArray
+    dzeta_dphi: xr.DataArray
 
-    def __post_init__(self, hhl: xr.DataArray):
+    @classmethod
+    def from_hhl(cls, dlon: float, dlat: float, hhl: xr.DataArray):
         dh_dx = destagger(diff.dx(hhl), "z")  # order is important
         dh_dy = destagger(diff.dy(hhl), "z")  # diff then destagger
         dh_dz = diff.dz_staggered(hhl)
 
-        self.sqrtg_r_s = -1 / dh_dz
-        self.dzeta_dlam = self.sqrtg_r_s / self.dlon * dh_dx
-        self.dzeta_dphi = self.sqrtg_r_s / self.dlat * dh_dy
+        sqrtg_r_s = -1 / dh_dz
+        dzeta_dlam = sqrtg_r_s / dlon * dh_dx
+        dzeta_dphi = sqrtg_r_s / dlat * dh_dy
+        return cls(dlon, dlat, sqrtg_r_s, dzeta_dlam, dzeta_dphi)
+
+    def __reduce__(self):
+        return self.from_dict, self.to_dict()
+
+    @classmethod
+    def from_dict(cls, obj: dict):
+        fmap = {f.name: f.type for f in dc.fields(cls)}
+        kwargs = {
+            key: value if fmap[key] != xr.DataArray else xr.DataArray.from_dict(value)
+            for key, value in obj.items()
+        }
+        return cls(**kwargs)
+
+    def to_dict(self) -> dict:
+        fmap = {f.name: f.type for f in dc.fields(self)}
+        return {
+            key: value if fmap[key] != xr.DataArray else value.to_dict("array")
+            for key, value in dc.asdict(self).items()
+        }
 
     def d_dlam(self, df_dx: xr.DataArray, df_dz: xr.DataArray) -> xr.DataArray:
         """Compute the derivative along the lambda axis."""
