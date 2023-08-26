7e0d33e32e8ee11f3fa0a2bf535f0564b02703e5
cosunae <carlos.osuna@meteoswiss.ch>
2023-08-26 23:29:10 +0200

    add relhum test

---
+++ tests/test_idpi/test_relhum.py
@@ -0,0 +1,20 @@
+# Third-party
+from numpy.testing import assert_allclose
+
+# First-party
+from idpi.operators.relhum import relhum
+from idpi.grib_decoder import GribReader
+
+
+def test_relhum(data_dir, fieldextra):
+    datafile = data_dir / "lfff00000000.ch"
+    cdatafile = data_dir / "lfff00000000c.ch"
+
+    reader = GribReader([cdatafile, datafile], ref_param="P")
+    ds = reader.load_cosmo_data(["P", "T", "QV"])
+
+    brn = relhum(ds["QV"], ds["T"], ds["P"], clipping=True, phase="water")
+
+    fs_ds = fieldextra("RELHUM")
+
+    assert_allclose(fs_ds["RELHUM"], brn, rtol=5e-3, atol=5e-2)
