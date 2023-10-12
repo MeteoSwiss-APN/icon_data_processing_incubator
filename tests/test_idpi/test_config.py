585820f579a30d4c334e650632adcbf4f532847f
cosunae <carlos.osuna@meteoswiss.ch>
2023-10-12 08:45:48 +0200

    implement contextmanager with decorator

--- tests/test_idpi/test_config.py
+++ tests/test_idpi/test_config.py
@@ -3,10 +3,13 @@ import idpi.config
 
 
 def test_config():
-    idpi.config.set(opt1=1)
-    assert idpi.config.get("opt1") == 1
+    with idpi.config.set_values(opt1=1):
+        assert idpi.config.get("opt1") == 1
 
-    with idpi.config.set(opt1=False, opt2=2):
-        assert not idpi.config.get("opt1")
+        with idpi.config.set_values(opt1=False, opt2=2):
+            assert not idpi.config.get("opt1")
+            assert idpi.config.get("opt2") == 2
 
-    assert idpi.config.get("opt1") == 1
+        assert idpi.config.get("opt1") == 1
+
+    assert not idpi.config.get("opt1")
