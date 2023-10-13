6347b94a19f2bdbee6537f889ab6e8b0e689db55
cosunae <carlos.osuna@meteoswiss.ch>
2023-10-13 08:14:29 +0200

    remove unused variable

--- src/idpi/system_definition.py
+++ src/idpi/system_definition.py
@@ -10,16 +10,6 @@ except KeyError:
     print("The FIELDEXTRA_PATH is not set, exiting")
     raise
 
-try:
-    input_data_directory = os.environ["INPUT_DATA_DIR"]
-except KeyError:
-    input_data_directory = str(
-        pathlib.Path(__file__).parent.parent.parent.resolve() / "test_data"
-    )
-    print(f"The INPUT_DATA_DIR is not set, setting it to {input_data_directory}")
-
-
 FX_BINARY = fieldextra_executable
-INPUT_DATA_DIR = input_data_directory
 
 root_dir = (pathlib.Path(os.path.dirname(os.path.abspath(__file__))) / "..").resolve()
