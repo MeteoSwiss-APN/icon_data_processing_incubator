fe7c67d7a8ec9720694dd872f1427597f589c2ca
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2023-10-17 10:47:03 +0200

    short spack dir

--- tools/setup_spack.sh
+++ tools/setup_spack.sh
@@ -6,6 +6,7 @@ script_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 spack_c2sm_url=https://github.com/C2SM/spack-c2sm.git
 spack_c2sm_tag=v0.20.1.0
+spack_dir=s
 workspace=$(pwd)
 
 while getopts "w:" flag; do
@@ -21,15 +22,14 @@ if [[ $(git --version) =~ git\ version\ 1 ]]; then
     # assume that we are on tsa
     module load git
 fi
-git clone --depth 1 --recurse-submodules --shallow-submodules -b ${spack_c2sm_tag} ${spack_c2sm_url}
+git clone --depth 1 --recurse-submodules --shallow-submodules -b ${spack_c2sm_tag} ${spack_c2sm_url} ${spack_dir}
 
-. spack-c2sm/setup-env.sh
+. ${spack_dir}/setup-env.sh
 
 mkdir spack-env
 cp ${script_dir}/spack.yaml spack-env/
 spack env activate -p spack-env
 
-export PATH=$PATH:${workspace}/spack-c2sm/user-cache/bootstrap/store/bin
 spack install
 
 popd
