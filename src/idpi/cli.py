b39d445867f738e8493e987fc92c025ad859c802
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2024-02-01 16:36:06 +0100

    Add regrid group to cli

--- src/idpi/cli.py
+++ src/idpi/cli.py
@@ -1,9 +1,13 @@
 """Command line interface of idpi."""
+from pathlib import Path
+
 # Third-party
 import click
 
 # Local
 from . import __version__
+from . import grib_decoder
+from .operators import regrid
 
 
 def print_version(ctx, _, value: bool) -> None:
@@ -13,6 +17,7 @@ def print_version(ctx, _, value: bool) -> None:
         ctx.exit(0)
 
 
+@click.group()
 @click.option(
     "--version",
     "-V",
@@ -21,7 +26,35 @@ def print_version(ctx, _, value: bool) -> None:
     expose_value=False,
     callback=print_version,
 )
-@click.group()
 def main() -> None:
     """Console script for test_cli_project."""
     print("CLI for IDPI")
+
+
+RESAMPLING = {
+    "nearest": regrid.Resampling.nearest,
+    "bilinear": regrid.Resampling.bilinear,
+    "cubic": regrid.Resampling.cubic,
+}
+
+
+@main.command("regrid")
+@click.option("--crs", type=click.Choice(["geolatlon"]), default="geolatlon")
+@click.option("--resampling", type=click.Choice(list(RESAMPLING.keys())), default="nearest")
+@click.argument("infile")
+@click.argument("outfile")
+@click.argument("params")
+def regrid_cmd(crs: str, resampling: str, infile: Path, outfile: Path, params: str):
+    resampling_arg = RESAMPLING[resampling]
+    crs_str = regrid.CRS_ALIASES.get(crs, crs)
+    
+    reader = grib_decoder.GribReader.from_files([infile])
+    ds = reader.load_fieldnames(params.split(","))
+
+    with outfile.open("wb") as fout:
+        for field in ds.values():
+            src = regrid.RegularGrid.from_field(field)
+            dst = src.to_crs(crs_str)
+
+            field_out = regrid.regrid(field, dst, resampling_arg)
+            grib_decoder.save(field_out, fout)
